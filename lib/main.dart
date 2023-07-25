78bd928ade48e4ff148c76e7bcdb6fef154dd1d7
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2023-07-25 02:49:12 -0500

    feat: init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:dog_walking_app/pages/Onboarding.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Onboarding(),
+    );
+  }
+}
